52e843e28f12cc30d2901ba18e430de862c68b2c
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-10-27 12:30:31 +0100

    implement initially with some tests

--- src/sequelize_index.ts
+++ src/sequelize_index.ts
@@ -1,8 +1,21 @@
-import { Sequelize, DataTypes, Model } from 'sequelize';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Sequelize,
+  Model,
+  ModelDefined,
+  DataTypes,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  Association,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  Optional,
+} from 'sequelize';
 
 const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite',
-  storage: './restaurants-seq.sqlite',
+  storage: './database/restaurants-seq.sqlite',
 });
 
 export { sequelize, DataTypes, Model };
--- tests/sequelize_index.test.ts
+++ tests/sequelize_index.test.ts
@@ -1,12 +1,13 @@
 import { sequelize } from '../src/sequelize_index';
-import Restaurant from '../src/Restaurant';
-import Menu from '../src/Menu';
+import Restaurant from '../src/models/Restaurant';
+import { Menu } from '../src/models/Menu';
+import MenuItem from '../src/models/MenuItem';
 
 describe('Restaurant', () => {
   /**
    * Runs the code prior to all tests
    */
-  beforeAll(async () => {
+  beforeEach(async () => {
     // the 'sync' method will create tables based on the model class
     // by setting 'force:true' the tables are recreated each time the
     // test suite is run
@@ -20,9 +21,63 @@ describe('Restaurant', () => {
 
   test('restaurants have menus', async () => {
     const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' });
-    const menu = await Menu.create({ title: 'set 1' });
-    await restaurant.addMenu('Cheese Menu');
+    const menu1 = await Menu.create({
+      title: 'set 1',
+    });
+
+    await restaurant.addMenu(menu1);
+
+    await restaurant.save();
+
     const menus = await restaurant.getMenus();
+
     expect(menus[0].title).toBe('set 1');
+  });
+
+  test('restaurants can create menus', async () => {
+    const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' });
+    await restaurant.createMenu({
+      title: 'set 1',
+    });
+
+    await restaurant.save();
+
+    const menus = await restaurant.getMenus();
+
+    expect(menus[0].title).toBe('set 1');
+  });
+
+  test('menus have menuItems', async () => {
+    const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' });
+    const menu = await restaurant.createMenu({
+      title: 'set 1',
+    });
+
+    const menuItem = await MenuItem.create({ name: 'food 1', price: 500 });
+
+    await menu.addMenuItem(menuItem);
+
+    await menu.save();
+
+    const items = await menu.getMenuItems();
+
+    expect(items[0].name).toBe('food 1');
+    expect(items[0].menu_id).toBe(1);
+  });
+
+  test('menus can create menuItems', async () => {
+    const restaurant = await Restaurant.create({ name: 'Ronalds', image: 'http://some.image.url' });
+    const menu = await restaurant.createMenu({
+      title: 'set 1',
+    });
+
+    await menu.createMenuItem({ name: 'food 1', price: 500 });
+
+    await menu.save();
+
+    const items = await menu.getMenuItems();
+
+    expect(items[0].name).toBe('food 1');
+    expect(items[0].menu_id).toBe(1);
   });
 });
